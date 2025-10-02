e3ab6fb6ae8824c48f76d7fcc1240e602f472aac
crueter <crueter@eden-emu.dev>
2025-10-02 11:31:16 -0400

    fix openbsd unzip, omni x11
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- unix/omnios-deps.sh
+++ unix/omnios-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 
 pkg install wget libx11 unzip math cmake gnu-make gcc14 zstd libxext \
-	libxrandr libxfixes
+	libxrandr libxfixes x11-protocols libxrender
--- unix/openbsd-deps.sh
+++ unix/openbsd-deps.sh
@@ -4,4 +4,5 @@ pkg_add -u
 
 pkg_add cmake ninja bison gawk gsed wget \
 	libiconv libogg libvorbis flac libsndfile \
-	opus mpg123 pulseaudio libusb1-1.0.27 lxrandr
+	opus mpg123 pulseaudio libusb1-1.0.27 lxrandr\
+	unzip-6.0p18-iconv
