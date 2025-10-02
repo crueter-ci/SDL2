56e22b10e2317a72ea20c052c859d4c47b6be346
crueter <crueter@eden-emu.dev>
2025-10-02 11:32:09 -0400

    ninja
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- unix/omnios-deps.sh
+++ unix/omnios-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 
 pkg install wget libx11 unzip math cmake gnu-make gcc14 zstd libxext \
-	libxrandr libxfixes x11-protocols libxrender
+	libxrandr libxfixes x11-protocols libxrender ninja
